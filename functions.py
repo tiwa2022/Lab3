c2e9ec9c55c8a3c7f57712c901858258d9251c6a
tiwa2022 <97244287+tiwa2022@users.noreply.github.com>
2022-01-27 12:01:06 -0500

    commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+
+
+def kiloToMiles(nkilo):
+    return nkilo * 0.621371
+
+
+def grades(ngrade):
+    return print("Your failed this course")
+    return print("You passed with an A+")
+    return print("Your passed this course")
+
+
+def volume(diameter,height):
+     pi = 3.14159265359
+     return pi * diameter * height
+     diameter=float(input("Enter diameter: "))
+     height=float(input("Enter height: "))
+     print (volume(diameter,height))
+     
+    
+
---
+++ test_lab3.py
@@ -0,0 +1,26 @@
+import functions as f
+
+def test_0():
+    assert f.kiloToMiles(0) == 0
+def test_1():
+    assert f.kiloToMiles(1) == 0.621371
+
+
+
+import functions as q
+
+def test_50():
+    assert q.grades(0-50) == print ("You failed this course")
+def test_89():
+    assert q.grades(51-89) == print ("You passed this course")
+def test_100():
+    assert q.grades(90-100) == print ("You passed with an A+")
+
+
+
+import functions as z
+
+def test_00():
+    assert z.volume(0,0) == 0
+def test_28():
+    assert z.volume(3,3) == 28.274333882310003
